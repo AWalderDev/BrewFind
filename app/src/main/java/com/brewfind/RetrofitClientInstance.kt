994d825a30532ced05b687f3231d91c2b5b1c7ad
walderaj <walderaj@mail.uc.edu>
2021-03-06 01:54:31 -0500

    Create RetrofitClientInstance

---
+++ app/src/main/java/com/brewfind/RetrofitClientInstance.kt
@@ -0,0 +1,24 @@
+package com.brewfind
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClientInstance {
+    //declare variables for retrofit instance and base url for json endpoint
+    private var retrofit: Retrofit? = null
+    private val base_url = "https://api.openbrewerydb.org"
+
+    //build retrofit client instance
+    val retrofitInstance : Retrofit?
+        get() {
+            //has this object been created yet?
+            if (retrofit == null) {
+                //create it
+                retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl(base_url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+}
