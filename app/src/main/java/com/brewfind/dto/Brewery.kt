ca1752729c65de47318f01466b5617a2a061b3fd
walderaj <walderaj@mail.uc.edu>
2021-04-02 12:20:48 -0400

    fix brewery class serialized names and fix dependencies in gradle file

--- app/src/main/java/com/brewfind/dto/Brewery.kt
+++ app/src/main/java/com/brewfind/dto/Brewery.kt
@@ -2,8 +2,21 @@ package com.brewfind.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Brewery(@SerializedName("Name") var name: String, @SerializedName("brewery_type") var size: String, @SerializedName("street") var street: String) {
+data class Brewery(
+    @SerializedName("id") var id: String,
+    @SerializedName("name") var name: String,
+    @SerializedName("brewery_type") var size: String,
+    @SerializedName("street") var street: String,
+    @SerializedName("city") var city: String,
+    @SerializedName("state") var state: String,
+    @SerializedName("postal_code") var postalCode: String,
+    @SerializedName("country") var country: String,
+    @SerializedName("longitude") var longitude: String,
+    @SerializedName("latitude") var latitude: String,
+    @SerializedName("phone") var phone: String,
+    @SerializedName("website_url") var website: String
+    ) {
     override fun toString(): String {
-        return "$name $size $street"
+        return "$name $size $street $city $state $postalCode $website"
     }
 }
